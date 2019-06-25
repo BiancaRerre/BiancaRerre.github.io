f7b0f902f2ce389655c9abe74e128801d00d73bd
BiancaRerre <52195618+BiancaRerre@users.noreply.github.com>
2019-06-25 05:28:45 -0300

    Add files via upload

---
+++ standalone/dynmap_access.php
@@ -0,0 +1,5 @@
+<?php
+$tilespath = '/home/minecraft/multicraft/servers/server7192/plugins/dynmap/web/tiles/';
+$markerspath = '/home/minecraft/multicraft/servers/server7192/plugins/dynmap/web/tiles/';
+$webpath = '/home/minecraft/multicraft/servers/server7192/plugins/dynmap/web/';
+?>
